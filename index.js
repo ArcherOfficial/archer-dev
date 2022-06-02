b791f755214f4b10ea63a483054de77c4fdcbc4d
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-02 10:08:28 +0700

    added coc client

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({
     allowedMentions: {
-        parse: ['users', 'roles'],
+        parse: ['users', 'roles', 'everyone'],
         repliedUser: false
     },
     intents: [
         Discord.GatewayIntentBits.Guilds,
         Discord.GatewayIntentBits.GuildMembers,
-        Discord.GatewayIntentBits.GuildMessages,
-        Discord.GatewayIntentBits.MessageContent
+        Discord.GatewayIntentBits.GuildMessages
     ]
 });
 
 // client configuration
+client.coc = new (require('clashofclans.js')).Client();
 client.commands = new Discord.Collection();
 client.config = require('./config.js');
 client.slashCommands = new Discord.Collection();
 
 client.on('error', (error) => console.error(error));
 client.on('warn', (info) => console.log(info));
+
+client.coc.login({
+    email: process.env.CLASHDEV_EMAIL, 
+    password: process.env.CLASHDEV_PASSWORD 
+}).then(() => {
+    console.log('[COC Client]:', 'Login to API ✅');
+}).catch(error => {
+    console.error('[COC Client]: Login failed!', error)
+});
 
 // loaded all handlers
 const fs = require('fs');
