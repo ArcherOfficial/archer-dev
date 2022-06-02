1e52ae6e911a55a54f71df8092bf95806237109f
deanprojectdc@gmail.com <deanprojectdc@gmail.com>
2022-06-02 10:35:16 +0700

    added eval

---
+++ commands/interaction/eval.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+
+module.exports = {
+    name: 'eval',
+    description: 'Evaluate JavaScript code',
+    options: [{
+        name: 'code',
+        description: 'JavaScript Code',
+        required: true,
+        type: 3
+    }],
+    execute: async (client, interaction) => {
+        const embed = new Discord.EmbedBuilder();
+        const code = interaction.options.getString('code');
+        try{
+            let evaled = await eval(code);
+            if (typeof evaled !== "string")
+                evaled = await require("util").inspect(evaled, { depth: 0 });
+
+            let output = clean(evaled);
+                embed.setDescription("```js\n" + output + "```")
+                embed.setColor('Blue');
+
+            if (!interaction.deferred) await interaction.reply({ embeds: [embed] });
+            await interaction.editReply({ embeds: [embed] });
+        } 
+        catch (Error){
+            let error = clean(Error);
+                embed.setDescription("```js\n" + error + "```")
+                embed.setTitle("Error!")
+                embed.setColor("Red");
+
+            if (!interacrion.deferred) await interaction.reply({ embeds: [embed] });
+            await interaction.editReply({ embeds: [embed] });
+        };
+    }
+};
+
+function clean(values) {
+    if(typeof values === "string") {
+        return values
+            .replace(/`/g, "`" + String.fromCharCode(8203))
+            .replace(/@/g, "@" + String.fromCharCode(8203));
+    }
+    else{
+        return values;
+    }
+};
--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,8 +5,8 @@ module.exports = (client, message) => {
     const content = message.content.toLowerCase();
     if (!content.startsWith(prefix)) return;
 
-    message.contentArguments = message.content.slice(prefix.length).trim().split(/ +/g) || [];
-    const cmd = message.contentArguments.shift().toLowerCase();
+    message.arguments = message.content.slice(prefix.length).trim().split(/ +/g) || [];
+    const cmd = message.arguments.shift().toLowerCase();
     if (cmd.length == 0) return;
 
     const command = client.commands.get(cmd);
